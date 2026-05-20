3eb2585d0037951179adc7608160a0008f59d369
Sand <sand999.com>
2026-05-20 21:45:05 +0300

    IPmask detection DONE TODO: Prefix

--- IP Calculator/resource.h
+++ IP Calculator/resource.h
@@ -2,15 +2,14 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDD_STATIC_INFO                 101
+#define IDD_DIALOG_MAIN                 101
 #define IDC_STATIC_IP_ADDRESS           1001
 #define IDC_STATIC_IP_MASK              1002
 #define IDC_IP_ADDRESS                  1003
 #define IDC_IP_MASK                     1004
 #define IDC_EDIT_PREFIX                 1005
 #define IDC_SPIN_PREFIX                 1007
-#define IDC_STATIC_                     1009
-#define IDC_STATIC_FRAME                1009
+#define IDC_STATIC_FRAME2               1010
 
 // Next default values for new objects
 // 
