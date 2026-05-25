fea508af862c8b7ff1fdb112d79721115fbaa83f
Sand <sand999.com>
2026-05-25 21:49:03 +0300

    IPcalc almost DONE

--- IP Calculator/main.cpp
+++ IP Calculator/main.cpp
@@ -3,15 +3,18 @@
 #include <CommCtrl.h>
 #include <iostream>
 #include "resource.h"
-//#include <stdio.h>
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, (DLGPROC)DlgProc, NULL);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDC_DIALOG_MAIN), NULL, (DLGPROC)DlgProc, NULL);
 	return 0;
 }
+
+LPSTR FormatAddress(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwIPaddress);
+LPSTR FormatCount(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwCount);
+VOID PrintInfo(HWND hwnd);
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -92,7 +95,15 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 	}
 	break;
+	case IDC_BUTTON_RESET:
+	{
+		SendMessage(hIPaddress, IPM_CLEARADDRESS, 0, 0);
+		SendMessage(hIPmask, IPM_CLEARADDRESS, 0, 0);
+		SendMessage(hIPprefix, WM_SETTEXT, 0, (LPARAM)"");
+		break;
+	}
 	case IDOK:
+		PrintInfo(hwnd);
 		break;
 	case IDCANCEL:
 		EndDialog(hwnd, 0);
@@ -125,4 +136,50 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	return FALSE;
+}
+LPSTR FormatAddress(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwIPaddress)
+{
+	sprintf
+	(
+		szBuffer,
+		"%s%i.%i.%i.%i",
+		szMessage,
+		FIRST_IPADDRESS(dwIPaddress),
+		SECOND_IPADDRESS(dwIPaddress),
+		THIRD_IPADDRESS(dwIPaddress),
+		FOURTH_IPADDRESS(dwIPaddress)
+	);
+	return szBuffer;
+}
+LPSTR FormatCount(CHAR szBuffer[], CONST CHAR szMessage[], DWORD dwCount)
+{
+	sprintf(szBuffer, "%s%i", szMessage, dwCount);
+	return szBuffer;
+}
+VOID PrintInfo(HWND hwnd) 
+{
+	HWND hIPaddress = GetDlgItem(hwnd, IDC_IP_ADDRESS);
+	HWND hIPmask = GetDlgItem(hwnd, IDC_IP_MASK);
+	HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+	DWORD dwIPaddress = 0;
+	DWORD dwIPmask = 0;
+	SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+	SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+	DWORD dwNetworkAddress = dwIPaddress & dwIPmask;
+	DWORD dwBroadcastAddress = dwIPaddress | ~dwIPmask;
+	CHAR szInfo[1024] = {};
+	CHAR szNetworkAddress[1024] = {};
+	CHAR szBroadcastAddress[1024] = {};
+	CHAR szIPcount[1024] = {};
+	CHAR szHostCount[1024] = {};
+	sprintf
+	(
+		szInfo,
+		"%s\n%s\n%s\n%s",
+		FormatAddress(szNetworkAddress,"Адрес сети:\t\t\t", dwIPaddress & dwIPmask),
+		FormatAddress(szBroadcastAddress, "Широковещательный адрес:\t", dwIPaddress | ~dwIPmask),
+		FormatCount(szIPcount, "Колличество IP-адресов:\t", dwBroadcastAddress - dwNetworkAddress + 1),
+		FormatCount(szIPcount, "Колличество узлов:\t", dwBroadcastAddress - dwNetworkAddress - 1)
+	);
+	SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)szInfo);
 }
